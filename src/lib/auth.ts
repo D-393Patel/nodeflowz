915b8e335b8f5f24f82a5b58d5fabee563641025
D-393Patel <pateldeepa017@gmail.com>
2026-01-23 17:14:32 +0530

    10: payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, check } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db"
-
+import { checkout, portal, polar } from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
 export const auth = betterAuth({
 database:prismaAdapter(prisma,{
     provider:"postgresql",
@@ -10,4 +11,23 @@ emailAndPassword:{
     enabled:true,
     autoSignIn:true, //this would automatically sign in when someone registers
 },
+plugins:[
+    polar({
+        client:polarClient,
+        createCustomerOnSignUp:true,
+        use:[
+            checkout({
+                products:[
+                    {
+                        productId:"f3c6b1f5-ef4e-403d-9dae-63e813662787",
+                        slug:"pro"
+                    }
+                ],
+                successUrl:process.env.POLAR_SUCCESS_URL,
+                authenticatedUsersOnly:true,
+            }),
+            portal(),
+        ]
+    })
+]
 });
