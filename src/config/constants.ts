3092f914891dd65e0e9828e67785e024b5fc42b3
D-393Patel <pateldeepa017@gmail.com>
2026-01-24 15:23:20 +0530

    13: workflows-ui

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,8 @@
 export const PAGINATION ={
     DEFAULT_PAGE:1,
-    DEFAULT_PAGE_SIZE:1,
+    DEFAULT_PAGE_SIZE:5,
     MAX_PAGE_SIZE:100,
-    MIN_pAGE_SIZE:5,
+    MIN_PAGE_SIZE:5,
 };
 
 //https://localhost:3000/workflows?search=plain
