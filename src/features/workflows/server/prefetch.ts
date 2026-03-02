2df73c83cf809bd38a5a8e39300f9b1e87e9c548
D-393Patel <pateldeepa017@gmail.com>
2026-03-02 16:53:51 +0530

    14: workflow-page

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -1,5 +1,7 @@
 import type { inferInput } from "@trpc/tanstack-react-query";
 import { prefetch ,trpc } from "@/trpc/server"; // adjust path if needed
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
 
 type Input = inferInput<typeof trpc.workflows.getMany>;
 
@@ -9,3 +11,17 @@ type Input = inferInput<typeof trpc.workflows.getMany>;
 export const prefetchWorkflows = (params: Input) => {
   return prefetch(trpc.workflows.getMany.queryOptions(params));
 };
+
+/**
+ * Prefetch a single workflow
+ */
+export const prefetchWorkflow=(id:string)=>{
+return prefetch(trpc.workflows.getOne.queryOptions({id}))
+};
+
+export const useSuspenseWorkflow=(id:string)=>{
+  const trpc=useTRPC();
+  return useSuspenseQuery(trpc.workflows.getOne.queryOptions({
+    id
+  }));
+}
