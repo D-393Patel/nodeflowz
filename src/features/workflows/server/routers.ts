49ff29b5356ee54c23166770bbf43b772ffc8e64
D-393Patel <pateldeepa017@gmail.com>
2026-03-05 16:56:56 +0530

    15:editor

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,4 +1,6 @@
 import { PAGINATION } from "@/config/constants";
+import { NodeType } from "@/generated/prisma";
+import {Node,Edge} from "@xyflow/react"
 import prisma from "@/lib/db"
 import { createTRPCRouter,premiumProcedure,protectedProcedure } from "@/trpc/init"
 import { createECDH } from "crypto"
@@ -11,7 +13,15 @@ export const workflowRouter =createTRPCRouter({
         return prisma.workflow.create({
             data:{
                 name:generateSlug(3),//3 words
-                userId:ctx.auth.user.id
+                userId:ctx.auth.user.id,
+                nodes:{
+                    create:{
+                        type:NodeType.INITIAL,
+                        position:{x:0,y:0},
+                        name:NodeType.INITIAL,
+                    },
+                    
+                },
             },
         });
     }),
@@ -37,10 +47,34 @@ export const workflowRouter =createTRPCRouter({
     
     getOne:protectedProcedure
     .input(z.object({id:z.string()}))
-    .query(({ctx,input})=>{
-        return prisma.workflow.findUniqueOrThrow({
-            where:{id:input.id,userId:ctx.auth.user.id}
+    .query(async ({ctx,input})=>{
+        const workflow=await prisma.workflow.findUniqueOrThrow({
+            where:{id:input.id,userId:ctx.auth.user.id},
+            include:{nodes:true,connections:true},
         })
+        //Transforming the server nodes to react flow compatible nodes
+        const nodes:Node[]= workflow.nodes.map((node)=>({
+            id:node.id,
+            type:node.type,
+            position:node.position as{x:number,y:number},
+            data:(node.data as Record<string,unknown>) || {},
+        }));
+
+        //Transforming the server connections to react flow compatible nodes
+        const edges:Edge[]=workflow.connections.map((connection)=>({
+        id:connection.id,
+        source:connection.fromNodeId,
+        target:connection.toNodeId,
+        sourceHandle:connection.fromOutput,
+        targetHandle:connection.toInput
+        }));
+
+        return {
+            id:workflow.id,
+            name:workflow.name,
+            nodes,
+            edges,
+    };
     }),
 
     getMany:protectedProcedure
